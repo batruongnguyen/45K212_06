a214cc52b8337e7a5449f0023ba27b23d2c59e5c
LuuBaoChau <baochau1232001@gmail.com>
2022-03-16 20:21:57 +0700

    footer

--- Source code/front-end/resoucres/js/scripts.js
+++ Source code/front-end/resoucres/js/scripts.js
@@ -1,5 +1,13 @@
-$(".hover").mouseleave(
-    function () {
-      $(this).removeClass("hover");
+$(document).ready(
+  function () {
+
+    // Scroll
+    $('a').click(function (event) {
+      $('html, body').animate({
+        scrollTop: $($.attr(this, 'href')).offset().top
+      }, 700);
+      event.preventDefault();
     }
-  );
+    )
+  }
+)
