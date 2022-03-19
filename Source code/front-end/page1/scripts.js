6823c8e1c4e72f08f657da705aa467a84bbb367c
LuuBaoChau <baochau1232001@gmail.com>
2022-03-20 02:15:10 +0700

    Điều hướng trang
    muốn kiểm tra hoạt động phải mở bằng live server

--- Source code/front-end/page1/scripts.js
+++ Source code/front-end/page1/scripts.js
@@ -9,5 +9,25 @@ $(document).ready(
       event.preventDefault();
     }
     )
+    
   }
+  
 )
+
+function check()
+    {
+        var question_01 = document.evaluate.rating3.value;
+        var question_02 = document.evaluate.rating3.value;
+        if(question_01 == "4")
+        {
+          window.location = "http://127.0.0.1:5500/page2.html";
+        }
+        else if(question_02 == "5")
+        {
+          window.location = "http://127.0.0.1:5500/page2.html";
+        }
+        else
+        {
+          window.location = "http://127.0.0.1:5500/page3.html";
+        }
+    }
