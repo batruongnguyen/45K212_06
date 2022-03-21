8c52ca7676f3c9bc749de6e2411f49f6882fdcfe
LuuBaoChau <baochau1232001@gmail.com>
2022-03-21 23:37:31 +0700

    đếm ký tự

--- SourceCode/front-end/general/scripts.js
+++ SourceCode/front-end/general/scripts.js
@@ -1,37 +1,40 @@
+/* ------ Scroll ----- */
 $(document).ready(
   function () {
-
-    // Scroll
     $('a').click(function (event) {
       $('html, body').animate({
         scrollTop: $($.attr(this, 'href')).offset().top
       }, 700);
       event.preventDefault();
-    }
-    )
-
+    })
   }
-
 )
 
+/*------ check -----*/
 function check() {
   var question_01 = document.evaluate.rating3.value;
   var question_02 = document.evaluate.rating3.value;
   var question_03 = document.evaluate.rating3.value;
-  if (question_01 == "4") {
+  let myContent = document.getElementById("info");
+  let count = (myContent.value).length;
+  if (question_01 == "4" && count>=25) {
     window.location = "http://127.0.0.1:5500/page2.html";
   }
-  else if (question_02 == "5") {
+  else if (question_02 == "5" && count>=25) {
     window.location = "http://127.0.0.1:5500/page2.html";
   }
-  else if(question_03 == "0"){
-    alert("Hãy đánh giá");
+  else if(question_03 == "0" && count>=25){
+    alert("Hãy để lại đánh giá sao cho chúng mình bạn nhé ^^");
+  }
+  else if(count<25){
+    alert("Hãy nhận xét cho chúng mình ít nhất 25 ký tự bạn nhé ^^");
   }
   else {
     window.location = "http://127.0.0.1:5500/page3.html";
   }
 }
 
+/*----- popup -----*/
 function popup() {
   let params =
     'width=window.innerWidth,height=window.innerHeight,menubar=no,toolbar=no,location=no,status=no,resizable=no,scrollbars=no'
