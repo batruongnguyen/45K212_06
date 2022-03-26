72cce87f0e84e1c183e52e6bb42db1035d23730d
NgBaTruong <85273668+batruongnguyen@users.noreply.github.com>
2022-03-26 14:18:42 +0700

    Create tempCodeRunnerFile.js

---
+++ SourceCode/back-end/src/tempCodeRunnerFile.js
@@ -0,0 +1,31 @@
+const path = require('path')
+const handlebars = require('express-handlebars')
+const express = require('express')
+const app = express()
+const morgan = require('morgan')
+const port = 3000
+
+// page1.css
+app.use(express.static(path.join(__dirname, 'public')))
+
+// http logger
+app.use(morgan('combined'))
+
+//template engine
+app.engine('handlebars', handlebars.engine({ extname: '.hbs', defaultLayout: "main"}))
+app.set('view engine', 'handlebars')
+app.set('views', './views')
+app.set('views', path.join(__dirname, 'resources/view'))
+
+app.get('/', (req, res) => {
+  res.render('home')
+})
+
+// Post method
+app.post("/#rating", (req, res) => {
+  
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
