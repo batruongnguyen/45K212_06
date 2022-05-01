c5dee6f81507a1081377b76b80b47e2b38698c74
LuuBaoChau <baochau1232001@gmail.com>
2022-05-01 16:55:30 +0700

    update js page1

--- SourceCode/front-end/resources/scripts.js
+++ SourceCode/front-end/resources/scripts.js
@@ -11,7 +11,7 @@ $(document).ready(
 )
 
 /*------ check -----*/
-/*
+
 function check() {
     var question_01 = document.evaluate.rating3.value;
     var question_02 = document.evaluate.rating3.value;
@@ -37,7 +37,7 @@ function check() {
     } else {
         window.location = "http://127.0.0.1:5500/page3.html";
     }
-}*/
+}
 
 /* ======== page 2 ========= */
 /*----- popup -----*/
@@ -63,55 +63,55 @@ function checkphone() {
 }
 
 /*--------------------------------*/
-document.getElementById("rating3-1").onclick = function(a) {
-    document.getElementById("info").style.display = 'initial';
-};
-document.getElementById("rating3-2").onclick = function() {
-    document.getElementById("info").style.display = 'initial';
-};
-document.getElementById("rating3-3").onclick = function() {
-    document.getElementById("info").style.display = 'initial';
-};
+// document.getElementById("rating3-1").onclick = function(a) {
+//     document.getElementById("info").style.display = 'initial';
+// };
+// document.getElementById("rating3-2").onclick = function() {
+//     document.getElementById("info").style.display = 'initial';
+// };
+// document.getElementById("rating3-3").onclick = function() {
+//     document.getElementById("info").style.display = 'initial';
+// };
 
-function fb() {
-    let params =
-        'width=window.innerWidth,height=window.innerHeight,menubar=no,toolbar=no,location=no,status=no,resizable=no,scrollbars=no'
-    open('https://www.facebook.com/BeautySpaDaNang/reviews/?ref=page_internal', 'test', params);
-    window.location = "http://127.0.0.1:5500/page5.html";
-};
-document.getElementById("rating3-4").onclick = function() {
-    Swal.fire({
-        text: 'Hãy đánh giá cho chúng mình trên fanpage dưới dạng đề xuất nhé',
-    })
-    setTimeout(fb, 3000)
-};
+// function fb() {
+//     let params =
+//         'width=window.innerWidth,height=window.innerHeight,menubar=no,toolbar=no,location=no,status=no,resizable=no,scrollbars=no'
+//     open('https://www.facebook.com/BeautySpaDaNang/reviews/?ref=page_internal', 'test', params);
+//     window.location = "http://127.0.0.1:5500/page5.html";
+// };
+// document.getElementById("rating3-4").onclick = function() {
+//     Swal.fire({
+//         text: 'Hãy đánh giá cho chúng mình trên fanpage dưới dạng đề xuất nhé',
+//     })
+//     setTimeout(fb, 3000)
+// };
 
-document.getElementById("rating3-5").onclick = function() {
-    Swal.fire({
-        text: 'Hãy đánh giá cho chúng mình trên fanpage dưới dạng đề xuất nhé',
-    })
-    setTimeout(fb, 3000)
-};
+// document.getElementById("rating3-5").onclick = function() {
+//     Swal.fire({
+//         text: 'Hãy đánh giá cho chúng mình trên fanpage dưới dạng đề xuất nhé',
+//     })
+//     setTimeout(fb, 3000)
+// };
 
 
-/*-----*/
-function check() {
-    var question = document.evaluate.rating3.value;
-    let myContent = document.getElementById("info");
-    let count = (myContent.value).length;
-    if (question == "0") {
-        Swal.fire({
-            icon: 'error',
-            title: 'Oops...',
-            text: 'Bạn đã quên đánh giá sao cho chúng mình?',
-        })
-    } else if (count < 25) {
-        Swal.fire({
-            icon: 'warning',
-            title: 'Oops...',
-            text: 'Hãy nhận xét cho chúng mình ít nhất 25 ký tự bạn nhé',
-        })
-    } else {
-        window.location = "http://127.0.0.1:5500/page3.html";
-    }
-}
+// /*-----*/
+// function check() {
+//     var question = document.evaluate.rating3.value;
+//     let myContent = document.getElementById("info");
+//     let count = (myContent.value).length;
+//     if (question == "0") {
+//         Swal.fire({
+//             icon: 'error',
+//             title: 'Oops...',
+//             text: 'Bạn đã quên đánh giá sao cho chúng mình?',
+//         })
+//     } else if (count < 25) {
+//         Swal.fire({
+//             icon: 'warning',
+//             title: 'Oops...',
+//             text: 'Hãy nhận xét cho chúng mình ít nhất 25 ký tự bạn nhé',
+//         })
+//     } else {
+//         window.location = "http://127.0.0.1:5500/page3.html";
+//     }
+// }
