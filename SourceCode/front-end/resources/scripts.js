2da9805f9b0fcceac96f7f4afff14ca6bf7f7139
NgBaTruong <85273668+batruongnguyen@users.noreply.github.com>
2022-05-02 21:46:13 +0700

    update back

--- SourceCode/front-end/resources/scripts.js
+++ SourceCode/front-end/resources/scripts.js
@@ -11,18 +11,16 @@ $(document).ready(
 )
 
 /*------ check -----*/
-
 function check() {
     var question_01 = document.evaluate.rating3.value;
-    var question_02 = document.evaluate.rating3.value;
-    var question_03 = document.evaluate.rating3.value;
+    var comments = document.getElementById("info").value;
     let myContent = document.getElementById("info");
     let count = (myContent.value).length;
     if (question_01 == "4" && count >= 25) {
-        window.location = "http://127.0.0.1:5500/page2.html";
-    } else if (question_02 == "5" && count >= 25) {
-        window.location = "http://127.0.0.1:5500/page2.html";
-    } else if (question_03 == "0") {
+        window.location = "../../ratingHandle.php?goal=page2.html&rating="+question_01+"&comments="+comments+"&name=null&phone=null";
+    } else if (question_01 == "5" && count >= 25) {
+        window.location = "../../ratingHandle.php?goal=page2.html&rating="+question_01+"&comments="+comments+"&name=null&phone=null";
+    } else if (question_01 == "0") {
         Swal.fire({
             icon: 'error',
             title: 'Oops...',
@@ -35,7 +33,9 @@ function check() {
             text: 'Hãy nhận xét cho chúng mình ít nhất 25 ký tự bạn nhé',
         })
     } else {
-        window.location = "http://127.0.0.1:5500/page3.html";
+        localStorage['star'] = question_01;
+        localStorage['comment'] = comments;
+        window.location = "../../page3.html";
     }
 }
 
@@ -50,12 +50,16 @@ function popup() {
 /* ======== page 3 ========= */
 /*----- check phone -----*/
 function checkphone() {
+    var local_star = localStorage['star'];
+    var local_comment = localStorage['comment'];
+    let name = document.getElementById("name").value;
     let myContent = document.getElementById("name");
     let count = (myContent.value).length;
     var regExp = /^(0[1-9][0-9]{8}|1[89]00[0-9]{4})$/;
     var phone = document.getElementById("phone").value;
     if (regExp.test(phone) && (count > 0))
-        window.location = "http://127.0.0.1:5500/page5.html";
+        // window.location = "../../page5.html";
+        window.location = "../../ratingHandle.php?goal=page5.html&rating="+local_star+"&comments="+local_comment+"&name="+name+"&phone="+phone;
     else if (count == 0) {
         Swal.fire({
             icon: 'warning',
@@ -71,7 +75,7 @@ function checkphone() {
 }
 
 function checka() {
-    window.location = "http://127.0.0.1:5500/page5.html";
+    window.location = "../../page5.html";
 }
 /*--------------------------------*/
 // document.getElementById("rating3-1").onclick = function(a) {
