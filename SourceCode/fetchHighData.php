9775d5890494e1d0b20193c4ac9f9d76aac92c99
NgBaTruong <85273668+batruongnguyen@users.noreply.github.com>
2022-05-03 23:18:14 +0700

    Update fetchHighData.php

--- SourceCode/fetchHighData.php
+++ SourceCode/fetchHighData.php
@@ -1,7 +1,7 @@
  <?php
     $link =  mysqli_connect("192.18.142.21:3306","team","Team12345678@") or die ("an error has been found: ".mysqli_error());
     $db_selected = mysqli_select_db($link,'beautyspa');
-    $sql1 = "SELECT * FROM Web_review WHERE star > 3 Order by ID DESC LIMIT 10;";
+    $sql1 = "SELECT * FROM Web_review WHERE star > 3 Order by ID DESC LIMIT 12;";
     $fetch = mysqli_query($link,$sql1);
     $result = mysqli_fetch_all($fetch);
     foreach($result as $key => &$record){
