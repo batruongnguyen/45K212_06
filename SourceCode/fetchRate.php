2b1e581b38fe1c9bf9589a9db0f8b3f6ea10c9b8
thanhrhy1 <fuzngo@gmail.com>
2022-05-04 18:03:56 +0700

    rating

--- SourceCode/fetchRate.php
+++ SourceCode/fetchRate.php
@@ -1,3 +1,17 @@
 <?php
-
+    $link =  mysqli_connect("192.18.142.21:3306","team","Team12345678@") or die ("an error has been found: ".mysqli_error());
+    $db_selected = mysqli_select_db($link,'beautyspa');
+    $sql1 = "SELECT * FROM Web_review Order by ID DESC LIMIT 20;";
+    $fetch = mysqli_query($link,$sql1);
+    $result = mysqli_fetch_all($fetch);
+    foreach($result as $key => &$record){
+        $sql2 = "SELECT username,phone FROM Users where user_id='".$record[1]."'";
+        $fetch2 = mysqli_query($link,$sql2);
+        array_push($record, mysqli_fetch_array($fetch2));
+    }
+    echo json_encode($result);
+    mysqli_free_result($fetch);
+    mysqli_close($link);
+    // header('Content-Type: application/json; charset=utf-8');
+    exit();
 ?>
